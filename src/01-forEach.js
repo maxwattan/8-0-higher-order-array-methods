1c192d91a0943142f3dc115c6860412c58138f59
MaxWattan <mcwattanac@gmail.com>
2022-11-29 20:57:20 -0500

    finished

--- src/01-forEach.js
+++ src/01-forEach.js
@@ -12,7 +12,9 @@ const exampleSongData = require("../data/songs");
  * Logs out all of the song titles.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  */
-function printAllSongTitles(songs) {}
+function printAllSongTitles(songs) {
+  return songs.forEach(song => console.log(song.title))
+}
 
 /**
  * Logs out the song title and song artist.
@@ -24,13 +26,17 @@ function printAllSongTitles(songs) {}
  *  //> "Up by Sebastian Kamae"
  *  //> ...
  */
-function printSongDetails(songs) {}
+function printSongDetails(songs) {
+  return songs.forEach(song => console.log(`${song.title} by ${song.artist}`))
+}
 
 /**
  * Logs out all of the song titles which have a runtime over three minutes.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  */
-function printSongTitlesOverThreeMinutes(songs) {}
+function printSongTitlesOverThreeMinutes(songs) {
+  return songs.forEach(song => console.log(song.title), (songs.runtimeInSeconds > 180))
+}
 
 module.exports = {
   printAllSongTitles,
